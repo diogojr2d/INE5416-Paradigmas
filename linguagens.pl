16be58572d033f546f051efd231ed1e81085bcef
Diogo Junior de Souza <diogojrdesouza@gmail.com>
2017-08-17 08:48:03 -0300

    Parser for diagrama_de_linguagens

---
+++ linguagens.pl
@@ -0,0 +1,267 @@
+linguagem('Scheme', 1975).
+linguagem('Modula', 1975).
+linguagem('CLU', 1974).
+linguagem('Smalltalk 74', 1974).
+linguagem('Scheme R5RS', 1998).
+linguagem('C++ (ISO)', 1998).
+linguagem('Java 2', 1998).
+linguagem('Haskell 98', 1998).
+linguagem('D 1.0', 2007).
+linguagem('C# 3.0', 2007).
+linguagem('Fortress 1.0beta', 2007).
+linguagem('GHC 5.00', 2001).
+linguagem('Smalltalk 72', 1972).
+linguagem('Scheme R4RS', 1988).
+linguagem('Erlang', 1988).
+linguagem('Life', 1988).
+linguagem('Lua', 1994).
+linguagem('Common Lisp (ANSI)', 1994).
+linguagem('Perl 5', 1994).
+linguagem('Mercury', 1993).
+linguagem('AppleScript', 1993).
+linguagem('R', 1993).
+linguagem('Java 7', 2011).
+linguagem('Lua 4.0', 2000).
+linguagem('Python 2.0', 2000).
+linguagem('C#', 2000).
+linguagem('Python 2.5', 2006).
+linguagem('Java 6', 2006).
+linguagem('CAL (Open Quark)', 2006).
+linguagem('Smalltalk 78', 1978).
+linguagem('Scheme MIT', 1978).
+linguagem('CSP', 1978).
+linguagem('C (K&R)', 1978).
+linguagem('B', 1970).
+linguagem('Prolog', 1970).
+linguagem('Squeak', 1996).
+linguagem('OCaml', 1996).
+linguagem('C++ (ARM)', 1990).
+linguagem('Haskell', 1990).
+linguagem('SML 90', 1990).
+linguagem('Fortran 90', 1990).
+linguagem('C 95', 1995).
+linguagem('Prolog ISO', 1995).
+linguagem('Ada 95', 1995).
+linguagem('PHP', 1995).
+linguagem('Java', 1995).
+linguagem('Ruby', 1995).
+linguagem('Modula-2', 1977).
+linguagem('ML', 1977).
+linguagem('Fortran 77', 1977).
+linguagem('Gofer', 1991).
+linguagem('Python', 1991).
+linguagem('Perl 4', 1991).
+linguagem('Oak', 1991).
+linguagem('Rust', 2010).
+linguagem('Haskell 2010', 2010).
+linguagem('Pascal', 1971).
+linguagem('C', 1971).
+linguagem('Python 3.0', 2008).
+linguagem('Python 3.6', 2016).
+linguagem('Smalltalk 76', 1976).
+linguagem('S', 1976).
+linguagem('Go', 2009).
+linguagem('C# 2.0', 2005).
+linguagem('CPL', 1961).
+linguagem('Ada 83', 1983).
+linguagem('Pascal AFNOR', 1983).
+linguagem('Fortran II', 1958).
+linguagem('Algol 58', 1958).
+linguagem('PL/I', 1964).
+linguagem('Basic', 1964).
+linguagem('Ada', 1979).
+linguagem('Simula I', 1963).
+linguagem('COBOL', 1960).
+linguagem('Algol 60', 1960).
+linguagem('Algol W', 1966).
+linguagem('.NET', 1999).
+linguagem('Python 1.5.2', 1999).
+linguagem('C 99', 1999).
+linguagem('Simula 67', 1967).
+linguagem('Common Lisp', 1985).
+linguagem('Miranda', 1985).
+linguagem('C++', 1985).
+linguagem('CLIPS', 1985).
+linguagem('Scheme R3RS', 1986).
+linguagem('Objective-C', 1986).
+linguagem('Eiffel', 1986).
+linguagem('Flow-matic', 1957).
+linguagem('Fortran I', 1957).
+linguagem('Fortran IV', 1962).
+linguagem('Python 2.4', 2004).
+linguagem('Java 5', 2004).
+linguagem('Scala', 2004).
+linguagem('Swift', 2014).
+linguagem('Java 8', 2014).
+linguagem('Python 2.2', 2002).
+linguagem('ABC', 1980).
+linguagem('Smalltalk 80', 1980).
+linguagem('CLOS', 1989).
+linguagem('Modula-3', 1989).
+linguagem('ANSI C', 1989).
+linguagem('Lambda Prolog', 1989).
+linguagem('Lisp', 1959).
+linguagem('Lazy ML', 1984).
+linguagem('SML', 1984).
+linguagem('Scheme 84', 1984).
+linguagem('BCPL', 1969).
+linguagem('Smalltalk', 1969).
+linguagem('Algol 68', 1968).
+linguagem('Lua 5.0', 2003).
+linguagem('Io', 2003).
+linguagem('Fortran 2003', 2003).
+linguagem('GHC 6.0', 2003).
+
+predecessora('GHC 6.0','Haskell 2010').
+predecessora('C++','Rust').
+predecessora('Erlang','Rust').
+predecessora('OCaml','Rust').
+predecessora('Haskell 98','Rust').
+predecessora('C# 2.0','Rust').
+predecessora('Lua 4.0','Lua 5.0').
+predecessora('Ada 83','Ada 95').
+predecessora('Scheme 84','Scheme R3RS').
+predecessora('PL/I','Pascal').
+predecessora('Algol W','Pascal').
+predecessora('Python 2.0','Python 2.2').
+predecessora('CPL','BCPL').
+predecessora('ML','SML').
+predecessora('Ada','Ada 83').
+predecessora('C++ (ARM)','C++ (ISO)').
+predecessora('Fortran IV','Basic').
+predecessora('Java 5','Java 6').
+predecessora('Smalltalk 76','Smalltalk 78').
+predecessora('SML 90','Mercury').
+predecessora('Prolog','Mercury').
+predecessora('Haskell','Mercury').
+predecessora('Modula-3','Python').
+predecessora('ANSI C','Python').
+predecessora('ABC','Python').
+predecessora('Algol 60','Scheme').
+predecessora('Lisp','Scheme').
+predecessora('Modula','Modula-2').
+predecessora('Pascal','CLU').
+predecessora('Simula 67','CLU').
+predecessora('Common Lisp','CLOS').
+predecessora('ML','Miranda').
+predecessora('Smalltalk 80','AppleScript').
+predecessora('Java','Java 2').
+predecessora('Perl 4','Perl 5').
+predecessora('Python 2.2','Python 2.4').
+predecessora('Lua','Io').
+predecessora('Smalltalk 80','Io').
+predecessora('Scheme MIT','Common Lisp').
+predecessora('Lisp','Common Lisp').
+predecessora('Python','Python 1.5.2').
+predecessora('Fortran I','Fortran II').
+predecessora('C#','D 1.0').
+predecessora('Java','D 1.0').
+predecessora('C 99','D 1.0').
+predecessora('C++','D 1.0').
+predecessora('BCPL','B').
+predecessora('Python 2.4','Python 2.5').
+predecessora('Flow-matic','COBOL').
+predecessora('Smalltalk','Smalltalk 72').
+predecessora('Haskell 98','Swift').
+predecessora('CLU','Swift').
+predecessora('Objective-C','Swift').
+predecessora('C# 2.0','Swift').
+predecessora('D 1.0','Swift').
+predecessora('Ruby','Swift').
+predecessora('Python 2.0','Swift').
+predecessora('Rust','Swift').
+predecessora('Haskell','Gofer').
+predecessora('Algol 58','Algol 60').
+predecessora('Smalltalk 80','Squeak').
+predecessora('Python 2.5','Python 3.0').
+predecessora('C++','C++ (ARM)').
+predecessora('Ada 83','C++ (ARM)').
+predecessora('CLU','C++ (ARM)').
+predecessora('Java 7','Java 8').
+predecessora('Scheme R4RS','Scheme R5RS').
+predecessora('C# 2.0','C# 3.0').
+predecessora('Common Lisp','Erlang').
+predecessora('Prolog','Erlang').
+predecessora('Scheme R3RS','Scheme R4RS').
+predecessora('Lazy ML','Haskell').
+predecessora('Miranda','Haskell').
+predecessora('Common Lisp','Common Lisp (ANSI)').
+predecessora('Smalltalk 74','Smalltalk 76').
+predecessora('COBOL','PL/I').
+predecessora('Algol 60','PL/I').
+predecessora('Fortran IV','PL/I').
+predecessora('Prolog','Prolog ISO').
+predecessora('ANSI C','C 95').
+predecessora('Algol 60','Algol 68').
+predecessora('Pascal AFNOR','Lua').
+predecessora('Perl 4','Lua').
+predecessora('Fortran I','Algol 58').
+predecessora('Algol 60','Algol W').
+predecessora('Algol 60','Simula I').
+predecessora('C++ (ARM)','Oak').
+predecessora('Fortran IV','Fortran 77').
+predecessora('Fortran 2003','Fortress 1.0beta').
+predecessora('Scala','Fortress 1.0beta').
+predecessora('Gofer','Haskell 98').
+predecessora('Haskell','Haskell 98').
+predecessora('Smalltalk 72','Smalltalk 74').
+predecessora('Pascal','Modula').
+predecessora('Java 6','Java 7').
+predecessora('Python 3.0','Python 3.6').
+predecessora('OCaml','Scala').
+predecessora('.NET','Scala').
+predecessora('Java 2','Scala').
+predecessora('Prolog','Life').
+predecessora('Smalltalk 80','Ruby').
+predecessora('Perl 4','Ruby').
+predecessora('Python','Ruby').
+predecessora('Java 2','Java 5').
+predecessora('Java 2','CAL (Open Quark)').
+predecessora('Haskell 98','CAL (Open Quark)').
+predecessora('C 95','C 99').
+predecessora('Simula 67','Eiffel').
+predecessora('Ada 83','Eiffel').
+predecessora('Python 1.5.2','Python 2.0').
+predecessora('B','C').
+predecessora('Algol 68','C').
+predecessora('Scheme','Scheme MIT').
+predecessora('C++','ANSI C').
+predecessora('C (K&R)','ANSI C').
+predecessora('Modula-2','Modula-3').
+predecessora('Haskell 98','GHC 5.00').
+predecessora('Lua','Lua 4.0').
+predecessora('S','R').
+predecessora('Scheme','R').
+predecessora('Common Lisp','R').
+predecessora('Algol 60','CPL').
+predecessora('Smalltalk 80','Objective-C').
+predecessora('C (K&R)','Objective-C').
+predecessora('Perl 5','PHP').
+predecessora('Oak','Java').
+predecessora('Simula 67','Smalltalk').
+predecessora('Fortran 90','Fortran 2003').
+predecessora('Scheme MIT','Scheme 84').
+predecessora('C#','C# 2.0').
+predecessora('C','C (K&R)').
+predecessora('Smalltalk 78','Smalltalk 80').
+predecessora('C (K&R)','C++').
+predecessora('Algol 68','C++').
+predecessora('Simula 67','C++').
+predecessora('Simula I','Simula 67').
+predecessora('GHC 5.00','GHC 6.0').
+predecessora('Java 2','C#').
+predecessora('C++ (ISO)','C#').
+predecessora('Fortran 77','Fortran 90').
+predecessora('CSP','Ada').
+predecessora('Pascal','Ada').
+predecessora('Prolog','CLIPS').
+predecessora('Prolog','Lambda Prolog').
+predecessora('SML','Lambda Prolog').
+predecessora('Fortran II','Fortran IV').
+predecessora('Python 2.0','Go').
+predecessora('C','Go').
+predecessora('CSP','Go').
+predecessora('Smalltalk 80','Go').
+predecessora('Modula-3','Go').
+predecessora('Pascal','Go').
+predecessora('C (K&R)','ANSI C').
---
+++ parser.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+vector<string> split(string str, char c = ' ')
+{
+    vector<string> result;
+
+    stringstream ss;
+    ss << str;
+
+    string newstr;
+
+    while (getline(ss, newstr, c)) {
+        result.push_back(newstr);
+    }
+
+    return result;
+}
+
+int main()
+{
+    ifstream entrada;
+    ofstream saida;
+    string linha;
+
+    entrada.open("lista.txt");
+    saida.open("linguagens.pl");
+
+    bool inicio = true;
+    while(!entrada.eof()) {
+        getline(entrada, linha);
+
+        if (linha.compare("fim") == 0) {
+            inicio = false;
+            saida << endl;
+            continue;
+        }
+
+        vector<string> palavra = split(linha, '\"');
+        if (inicio) {
+            for (int i = 2; i < palavra.size(); i++) {
+                saida << "linguagem(\'" << palavra[i] << "\', " << palavra[1] << ")." << endl;
+            }
+        } else {
+            saida << "predecessora(\'" << palavra[1] << "\',\'" << palavra[3] << "\')." << endl;
+        }
+    }
+
+    entrada.close();
+    saida.close();
+
+    cout << "Fim do Programa" << endl;
+    return 0;
+}
